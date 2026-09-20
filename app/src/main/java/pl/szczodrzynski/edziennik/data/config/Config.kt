47c490feb4315b9dfc5dfb1e5290b2d69bd90432
konikblabla-cyber <konikblabla@gmail.com>
2026-09-20 04:49:44 +0200

    Default UI to dark mode for new installations

--- app/src/main/java/pl/szczodrzynski/edziennik/data/config/Config.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/data/config/Config.kt
@@ -71,7 +71,7 @@ class Config(app: App) : BaseConfig<Config>(app, profileId = null) {
         var themeColor by config<Theme>(Theme.DEFAULT)
         var themeType by config<Theme.Type>(Theme.Type.M3)
         var themeMode by config<Theme.Mode>(Theme.Mode.DAYNIGHT)
-        var themeNightMode by config<Boolean?>(null)
+        var themeNightMode by config<Boolean?>(true)
         var themeBlackMode by config<Boolean>(false)
 
         var language by config<String?>(null)
