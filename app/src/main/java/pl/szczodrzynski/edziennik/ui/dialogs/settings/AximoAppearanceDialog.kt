35bbbd04d1fd85a07aef5c520332c9b1e77d3cfb
konikblabla-cyber <konikblabla@gmail.com>
2026-09-21 18:44:14 +0200

    fix: make Aximo appearance dialog compatible with SimpleDialog

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/dialogs/settings/AximoAppearanceDialog.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/dialogs/settings/AximoAppearanceDialog.kt
@@ -6,27 +6,15 @@ import pl.szczodrzynski.edziennik.R
 import pl.szczodrzynski.edziennik.ui.aximo.AximoAppearanceStyle
 import pl.szczodrzynski.edziennik.ui.base.dialog.SimpleDialog
 
-class AximoAppearanceDialog(activity: AppCompatActivity) : SimpleDialog<Int>(activity, {}) {
-    init {
-        title(R.string.settings_aximo_styles_title)
-        message(R.string.settings_aximo_styles_subtitle)
-        single(
-            AximoAppearanceStyle.entries.mapIndexed { index, style ->
-                style.title to index
-            }.toMap(),
-            default = (activity.application as App).config.ui.aximoAppearanceStyle
-        )
-        positive(R.string.ok)
-        negative(R.string.cancel)
-    }
-
-    override suspend fun onPositiveClick(): Boolean {
-        val selected = getSingleSelection() ?: return DISMISS
-        val app = activity.application as App
-        if (app.config.ui.aximoAppearanceStyle != selected) {
-            app.config.ui.aximoAppearanceStyle = selected
-            activity.recreate()
-        }
-        return DISMISS
-    }
-}
+class AximoAppearanceDialog(activity: AppCompatActivity) : SimpleDialog<Int>(activity, {
+    title(R.string.settings_aximo_styles_title)
+    message(R.string.settings_aximo_styles_subtitle)
+    single(
+        AximoAppearanceStyle.entries.mapIndexed { index, style ->
+            style.title to index
+        }.toMap(),
+        default = (activity.application as App).config.ui.aximoAppearanceStyle
+    )
+    positive(R.string.ok)
+    negative(R.string.cancel)
+})
