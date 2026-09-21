42997305887e6852b92bf3718d6ee703a84d0579
konikblabla-cyber <konikblabla@gmail.com>
2026-09-21 15:28:00 +0200

    feat: restore Aximo home navigation and settings

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/settings/cards/SettingsAximoCard.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/settings/cards/SettingsAximoCard.kt
@@ -8,34 +8,21 @@ import com.danielstone.materialaboutlibrary.model.MaterialAboutCard
 import com.mikepenz.iconics.typeface.library.community.material.CommunityMaterial
 import pl.szczodrzynski.edziennik.R
 import pl.szczodrzynski.edziennik.ui.base.dialog.SimpleDialog
-import pl.szczodrzynski.edziennik.ui.dialogs.settings.AppLanguageDialog
 import pl.szczodrzynski.edziennik.ui.dialogs.settings.BellSyncConfigDialog
-import pl.szczodrzynski.edziennik.ui.dialogs.settings.NotificationFilterDialog
-import pl.szczodrzynski.edziennik.ui.dialogs.settings.QuietHoursConfigDialog
 import pl.szczodrzynski.edziennik.ui.dialogs.settings.ThemeChooserDialog
 import pl.szczodrzynski.edziennik.ui.dialogs.settings.TimetableConfigDialog
-import pl.szczodrzynski.edziennik.ui.home.HomeConfigDialog
 import pl.szczodrzynski.edziennik.ui.settings.SettingsCard
 import pl.szczodrzynski.edziennik.ui.settings.SettingsUtil
-import pl.szczodrzynski.edziennik.utils.models.Time
 
 class SettingsAximoCard(util: SettingsUtil) : SettingsCard(util) {
 
     override fun buildCard() = util.createCard(
-        R.string.settings_card_aximo_title,
+        R.string.settings_card_theme_title,
         items = ::getItems,
-        itemsMore = ::getItemsMore,
+        itemsMore = { emptyList() },
     )
 
     override fun getItems(card: MaterialAboutCard) = listOf(
-        util.createActionItem(
-            text = R.string.settings_aximo_home_text,
-            subText = R.string.settings_aximo_home_subtext,
-            icon = CommunityMaterial.Icon3.cmd_view_dashboard_outline
-        ) {
-            HomeConfigDialog(activity, reloadOnDismiss = true).show()
-        },
-
         util.createActionItem(
             text = R.string.settings_theme_theme_text,
             subText = app.uiManager.themeColor.nameRes,
@@ -45,26 +32,67 @@ class SettingsAximoCard(util: SettingsUtil) : SettingsCard(util) {
         },
 
         util.createActionItem(
-            text = R.string.settings_about_language_text,
-            subText = R.string.settings_about_language_subtext,
-            icon = CommunityMaterial.Icon3.cmd_translate
-        ) {
-            AppLanguageDialog(activity).show()
-        },
-
-        util.createActionItem(
             text = R.string.settings_theme_app_background_text,
             subText = R.string.settings_theme_app_background_subtext,
             icon = CommunityMaterial.Icon2.cmd_image_filter_hdr
         ) {
-            chooseAppBackground()
+            if (app.config.ui.appBackground == null) {
+                activity.requestHandler.requestAppBackground {
+                    activity.setAppBackground()
+                }
+            } else {
+                SimpleDialog<Int>(activity) {
+                    itemsRes(
+                        R.string.settings_theme_app_background_dialog_set to 0,
+                        R.string.settings_theme_app_background_dialog_restore to 1,
+                    ) {
+                        when (it) {
+                            0 -> activity.requestHandler.requestAppBackground {
+                                activity.setAppBackground()
+                            }
+                            1 -> {
+                                app.config.ui.appBackground = null
+                                activity.setAppBackground()
+                            }
+                        }
+                    }
+                    negative(R.string.cancel)
+                }.show()
+            }
         },
 
         util.createActionItem(
             text = R.string.settings_theme_drawer_header_text,
             icon = CommunityMaterial.Icon2.cmd_image_outline
         ) {
-            chooseHeaderBackground()
+            if (app.config.ui.headerBackground == null) {
+                activity.requestHandler.requestHeaderBackground {
+                    activity.drawer.setAccountHeaderBackground(null)
+                    activity.drawer.setAccountHeaderBackground(app.config.ui.headerBackground)
+                    activity.drawer.open()
+                }
+            } else {
+                SimpleDialog<Int>(activity) {
+                    itemsRes(
+                        R.string.settings_theme_drawer_header_dialog_set to 0,
+                        R.string.settings_theme_drawer_header_dialog_restore to 1,
+                    ) {
+                        when (it) {
+                            0 -> activity.requestHandler.requestHeaderBackground {
+                                activity.drawer.setAccountHeaderBackground(null)
+                                activity.drawer.setAccountHeaderBackground(app.config.ui.headerBackground)
+                                activity.drawer.open()
+                            }
+                            1 -> {
+                                app.config.ui.headerBackground = null
+                                activity.drawer.setAccountHeaderBackground(null)
+                                activity.drawer.open()
+                            }
+                        }
+                    }
+                    negative(R.string.cancel)
+                }.show()
+            }
         },
 
         util.createActionItem(
@@ -72,33 +100,6 @@ class SettingsAximoCard(util: SettingsUtil) : SettingsCard(util) {
             icon = CommunityMaterial.Icon.cmd_cog_outline
         ) {
             TimetableConfigDialog(activity).show()
-        },
-
-        util.createPropertyItem(
-            text = R.string.settings_sync_quiet_hours_text,
-            icon = CommunityMaterial.Icon.cmd_bell_sleep_outline,
-            value = configGlobal.sync.quietHoursEnabled
-        ) { _, value ->
-            configGlobal.sync.quietHoursEnabled = value
-        },
-
-        util.createPropertyItem(
-            text = R.string.settings_sync_quiet_during_lessons_text,
-            subText = R.string.settings_sync_quiet_during_lessons_subtext,
-            icon = CommunityMaterial.Icon3.cmd_school_outline,
-            value = configGlobal.sync.quietDuringLessons
-        ) { _, value ->
-            configGlobal.sync.quietDuringLessons = value
-        }
-    )
-
-    override fun getItemsMore(card: MaterialAboutCard) = listOf(
-        util.createActionItem(
-            text = R.string.settings_profile_notifications_text,
-            subText = R.string.settings_profile_notifications_subtext,
-            icon = CommunityMaterial.Icon2.cmd_filter_outline
-        ) {
-            NotificationFilterDialog(activity).show()
         },
 
         util.createActionItem(
@@ -109,78 +110,11 @@ class SettingsAximoCard(util: SettingsUtil) : SettingsCard(util) {
         },
 
         util.createPropertyItem(
-            text = R.string.settings_theme_mini_drawer_text,
-            subText = R.string.settings_theme_mini_drawer_subtext,
-            icon = CommunityMaterial.Icon.cmd_dots_vertical,
-            value = configGlobal.ui.miniMenuVisible
+            text = R.string.settings_sync_quiet_hours_text,
+            icon = CommunityMaterial.Icon.cmd_bell_sleep_outline,
+            value = configGlobal.sync.quietDuringLessons
         ) { _, value ->
-            configGlobal.ui.miniMenuVisible = value
-            activity.navView.drawer.miniDrawerVisiblePortrait = value
-        },
-
-        util.createPropertyItem(
-            text = R.string.settings_theme_open_drawer_on_back_pressed_text,
-            icon = CommunityMaterial.Icon3.cmd_menu_open,
-            value = configGlobal.ui.openDrawerOnBackPressed
-        ) { _, value ->
-            configGlobal.ui.openDrawerOnBackPressed = value
+            configGlobal.sync.quietDuringLessons = value
         }
     )
-
-    private fun chooseAppBackground() {
-        if (app.config.ui.appBackground == null) {
-            activity.requestHandler.requestAppBackground {
-                activity.setAppBackground()
-            }
-            return
-        }
-
-        SimpleDialog<Int>(activity) {
-            itemsRes(
-                R.string.settings_theme_app_background_dialog_set to 0,
-                R.string.settings_theme_app_background_dialog_restore to 1,
-            ) {
-                when (it) {
-                    0 -> activity.requestHandler.requestAppBackground {
-                        activity.setAppBackground()
-                    }
-                    1 -> {
-                        app.config.ui.appBackground = null
-                        activity.setAppBackground()
-                    }
-                }
-            }
-            negative(R.string.cancel)
-        }.show()
-    }
-
-    private fun chooseHeaderBackground() {
-        if (app.config.ui.headerBackground == null) {
-            setHeaderBackground()
-            return
-        }
-
-        SimpleDialog<Int>(activity) {
-            itemsRes(
-                R.string.settings_theme_drawer_header_dialog_set to 0,
-                R.string.settings_theme_drawer_header_dialog_restore to 1,
-            ) {
-                when (it) {
-                    0 -> setHeaderBackground()
-                    1 -> {
-                        app.config.ui.headerBackground = null
-                        activity.drawer.setAccountHeaderBackground(null)
-                        activity.drawer.open()
-                    }
-                }
-            }
-            negative(R.string.cancel)
-        }.show()
-    }
-
-    private fun setHeaderBackground() = activity.requestHandler.requestHeaderBackground {
-        activity.drawer.setAccountHeaderBackground(null)
-        activity.drawer.setAccountHeaderBackground(app.config.ui.headerBackground)
-        activity.drawer.open()
-    }
 }
