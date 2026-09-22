b22a5553ddda47ef4d37438421a9db3e25b26161
konikblabla-cyber <konikblabla@gmail.com>
2026-09-22 18:54:18 +0200

    fix: import Aximo appearance style for bottom navigation

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/main/AximoBottomNavigation.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/main/AximoBottomNavigation.kt
@@ -15,6 +15,7 @@ import com.mikepenz.iconics.utils.sizeDp
 import pl.szczodrzynski.edziennik.App
 import pl.szczodrzynski.edziennik.MainActivity
 import pl.szczodrzynski.edziennik.data.enums.NavTarget
+import pl.szczodrzynski.edziennik.ui.aximo.AximoAppearanceStyle
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.math.sqrt
