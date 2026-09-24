17371712f8982e147ba95a02cef328cf9554b0f0
konikblabla-cyber <konikblabla@gmail.com>
2026-09-24 10:29:04 +0200

    fix: remove duplicate refresh gesture branch

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/aximo/AximoSwipeRefreshLayout.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/aximo/AximoSwipeRefreshLayout.kt
@@ -62,14 +62,6 @@ class AximoSwipeRefreshLayout @JvmOverloads constructor(
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                 activeScrollableChild = null
             }
-
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                activeScrollableChild = null
-            }
-
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                activeScrollableChild = null
-            }
         }
 
         return super.onInterceptTouchEvent(ev)
