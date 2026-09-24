de99c249f95a1b1704c4f37b12b3e8373c0d7da0
konikblabla-cyber <konikblabla@gmail.com>
2026-09-24 10:28:42 +0200

    fix: reset active refresh target between gestures

--- app/src/main/java/pl/szczodrzynski/edziennik/ui/aximo/AximoSwipeRefreshLayout.kt
+++ app/src/main/java/pl/szczodrzynski/edziennik/ui/aximo/AximoSwipeRefreshLayout.kt
@@ -66,6 +66,10 @@ class AximoSwipeRefreshLayout @JvmOverloads constructor(
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                 activeScrollableChild = null
             }
+
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                activeScrollableChild = null
+            }
         }
 
         return super.onInterceptTouchEvent(ev)
